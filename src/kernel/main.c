270d67df5299b636cefda083eba5503b2188a22a
biosfood <mail@lukas-eisenhauer.de>
2022-06-12 12:19:15 +0200

    clean up syscall code

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -6,112 +6,53 @@
 #include <syscalls.h>
 #include <util.h>
 
-// todo: use data structures more suited to the job
-ListElement *services, *callsToProcess;
+extern ListElement *callsToProcess;
+extern void (*syscallHandlers[])(Syscall *);
 
-Service *findService(char *name) {
-    foreach (services, Service *, service, {
-        if (stringEquals(service->name, name)) {
-            return service;
-        }
-    })
-        ;
-    return NULL;
-}
+void loadAndScheduleLoader(void *multibootInfo) {
+    void *address = kernelMapMultiplePhysicalPages(multibootInfo, 4);
+    uint32_t tarSize = 0;
+    void *initrd = findInitrd(address, &tarSize);
+    void *loaderProgram = findTarFile(initrd, tarSize, "initrd/loader");
+    loadElf(loaderProgram, "loader");
 
-Service *findServiceByCR3(uint32_t cr3) {
-    foreach (services, Service *, service, {
-        if (getPhysicalAddressKernel(service->pagingInfo.pageDirectory) ==
-            PTR(cr3)) {
-            return service;
-        }
-    })
-        ;
-    return NULL;
-}
+    Service *loader = findService("loader");
+    Provider *provider = findProvider(loader, "main");
+    Syscall *runLoader = malloc(sizeof(Syscall));
+    runLoader->function = SYS_RUN;
+    runLoader->address = provider->address;
+    runLoader->resume = true;
+    runLoader->cr3 = getPhysicalAddressKernel(loader->pagingInfo.pageDirectory);
+    runLoader->esp = malloc(0x1000);
+    runLoader->respondingTo = NULL;
+    runLoader->service = loader;
+    memset(runLoader->esp, 0, 0x1000);
+    runLoader->esp += 0xFFC;
+    *(void **)runLoader->esp = runEnd;
+    sharePage(&loader->pagingInfo, runLoader->esp, runLoader->esp);
 
-Provider *findProvider(Service *service, char *name) {
-    foreach (service->providers, Provider *, provider, {
-        if (stringEquals(provider->name, name)) {
-            return provider;
-        }
-    })
-        ;
-    return NULL;
-}
-
-void runRequest(Provider *provider, void *parameter) {
-    run(provider->service, provider->address);
-}
-
-void handleInstallSyscall(Syscall *call, Service *service) {
-    Provider *provider = malloc(sizeof(Provider));
-    RegisterServiceProviderSyscall *registerCall = (void *)call;
-    char *providerName = kernelMapPhysical(
-        getPhysicalAddress(registerCall->pageDirectory, registerCall->name));
-    provider->name = providerName;
-    provider->address = registerCall->handler;
-    provider->service = service;
-    listAdd(&service->providers, provider);
-    call->resume = true;
-    listAdd(&callsToProcess, call);
-}
-
-extern void *runEnd;
-
-void handleRequestSyscall(Syscall *call, Service *service) {
-    RequestSyscall *request = (void *)call;
-    char *serviceName = kernelMapPhysical(
-        getPhysicalAddress(request->pageDirectory, request->service));
-    Service *providerService = findService(serviceName);
-    char *providerName = kernelMapPhysical(
-        getPhysicalAddress(request->pageDirectory, request->request));
-    Provider *callProvider = findProvider(providerService, providerName);
-    Syscall *runCall = malloc(sizeof(Syscall));
-    runCall->id = 0;
-    runCall->resume = true;
-    runCall->respondingTo = call;
-    runCall->pageDirectory = providerService->pagingInfo.pageDirectory;
-    runCall->cr3 = U32(getPhysicalAddressKernel(runCall->pageDirectory));
-    runCall->returnEsp = malloc(0x1000);
-    runCall->returnAddress = callProvider->address;
-    runCall->resume = true;
-    sharePage(&providerService->pagingInfo, runCall->returnEsp,
-              runCall->returnEsp);
-    runCall->returnEsp += 0xFFC;
-    *(void **)runCall->returnEsp = runEnd;
-    listAdd(&callsToProcess, runCall);
+    listAdd(&callsToProcess, runLoader);
 }
 
 void kernelMain(void *multibootInfo) {
     setupMemory();
-    void *address = kernelMapMultiplePhysicalPages(multibootInfo, 4);
-    uint32_t tarSize = 0;
-    void *initrd = findInitrd(address, &tarSize);
+    // loading the loader also reserves the needed space for the
+    // multiboot-loaded stuff
+    loadAndScheduleLoader(multibootInfo);
     setupSyscalls();
-    void *loaderProgram = findTarFile(initrd, tarSize, "initrd/loader");
-    loadElf(loaderProgram, "loader", &services);
-    Service *loader = findService("loader");
-    Provider *provider = findProvider(loader, "main");
-    runRequest(provider, NULL);
     while (1) {
         Syscall *call = listPopFirst(&callsToProcess);
         if (!call) {
             asm("hlt");
             continue;
         }
-        Service *service = findServiceByCR3(call->cr3);
         if (call->resume) {
             resume(call);
             continue;
         }
-        switch (call->id) {
-        case SYS_REGISTER_FUNCTION:
-            handleInstallSyscall(call, service);
-            break;
-        case SYS_REQUEST:
-            handleRequestSyscall(call, service);
-            break;
+        void (*handler)(Syscall *) = syscallHandlers[call->function];
+        if (handler) {
+            handler(call);
         }
     }
 }
--- src/kernel/syscalls/syscall.c
+++ src/kernel/syscalls/syscall.c
@@ -1,4 +1,5 @@
 #include <memory.h>
+#include <service.h>
 #include <stdint.h>
 #include <syscall.h>
 #include <syscalls.h>
@@ -20,15 +21,17 @@ void handleSyscall(void *cr3, Syscall *callData) {
     if (!callData) {
         asm("jmp runEndSyscall");
     }
-    callData->pageDirectory = kernelMapPhysical(cr3);
-    void *dataPhysical = getPhysicalAddress(callData->pageDirectory, callData);
+    void *dataPhysical =
+        getPhysicalAddress(currentService->pagingInfo.pageDirectory, callData);
     Syscall *data = kernelMapPhysical(dataPhysical);
-    data->cr3 = U32(cr3);
+    data->cr3 = cr3;
+    data->service = currentService;
     listAdd(&callsToProcess, data);
     asm("jmp runEndSyscall");
 }
 
 extern void(syscallStub)();
+
 void *syscallStubPtr = syscallStub;
 void *syscallStubPointer = &syscallStubPtr;
 
@@ -38,3 +41,44 @@ void setupSyscalls() {
     writeMsrRegister(0x176, syscallStubPtr);          // the handler
     return;
 }
+
+void handleInstallSyscall(Syscall *call) {
+    Provider *provider = malloc(sizeof(Provider));
+    RegisterServiceProviderSyscall *registerCall = (void *)call;
+    Service *service = call->service;
+    char *providerName = kernelMapPhysical(getPhysicalAddress(
+        service->pagingInfo.pageDirectory, registerCall->name));
+    provider->name = providerName;
+    provider->address = registerCall->handler;
+    provider->service = call->service;
+    listAdd(&service->providers, provider);
+    call->resume = true;
+    listAdd(&callsToProcess, call);
+}
+
+void handleRequestSyscall(Syscall *call) {
+    RequestSyscall *request = (void *)call;
+    Service *service = call->service;
+    char *serviceName = kernelMapPhysical(getPhysicalAddress(
+        service->pagingInfo.pageDirectory, request->serviceName));
+    Service *providerService = findService(serviceName);
+    char *providerName = kernelMapPhysical(getPhysicalAddress(
+        service->pagingInfo.pageDirectory, request->providerName));
+    Provider *callProvider = findProvider(providerService, providerName);
+    Syscall *runCall = malloc(sizeof(Syscall));
+    runCall->function = SYS_RUN;
+    runCall->address = callProvider->address;
+    runCall->esp = malloc(0x1000);
+    runCall->respondingTo = call;
+    runCall->cr3 =
+        getPhysicalAddressKernel(providerService->pagingInfo.pageDirectory);
+    runCall->service = providerService;
+    runCall->resume = true;
+    sharePage(&providerService->pagingInfo, runCall->esp, runCall->esp);
+    runCall->esp += 0xFFC;
+    *(void **)runCall->esp = runEnd;
+    listAdd(&callsToProcess, runCall);
+}
+
+void (*syscallHandlers[])(Syscall *) = {0, handleInstallSyscall,
+                                        handleRequestSyscall};
